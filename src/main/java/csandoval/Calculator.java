2e4b9fe7b60f449119dc2c73110854faa29c2c93
Carlos Sandoval <c.sandoval@globant.com>
2024-08-09 11:27:10 -0500

    feat(calculator): add sum and subtract operations

--- src/main/java/csandoval/Calculator.java
+++ src/main/java/csandoval/Calculator.java
@@ -1,5 +1,11 @@
 package csandoval;
 
 public class Calculator {
+    public static double sum(double op1, double op2) {
+        return op1 + op2;
+    }
 
+    public static double sub(double op1, double op2) {
+        return op1 - op2;
+    }
 }
