ab8079a6f33cc4826df40869962a0f389ba9ba1d
Carlos Sandoval <c.sandoval@globant.com>
2024-08-09 11:31:26 -0500

    feat(calculator): add multiplication and division operations

--- src/main/java/csandoval/Calculator.java
+++ src/main/java/csandoval/Calculator.java
@@ -1,5 +1,13 @@
 package csandoval;
 
 public class Calculator {
+    public static double mul(double op1, double op2) {
+        return op1 * op2;
+    }
 
+    public static double div(double op1, double op2) throws ArithmeticException {
+        if (op2 == 0.0)
+            throw new ArithmeticException("Div by zero error!");
+        return op1 / op2;
+    }
 }
